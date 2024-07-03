c91427cea4af61d3229e21d6f84afd59d60dd428
ahmedabdelraouf <ahmednsreldeen94@gmail.com>
2024-07-04 02:55:58 +0300

    first crop prediction

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::post('/predict-crop', [AuthController::class, 'predictCrop']);
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working'], 200);
